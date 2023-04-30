dacadd3a04e7b66189df7990ba67c4f242087d24
Romulo-joy <110550226+Romulo-joy@users.noreply.github.com>
2023-04-30 17:46:54 -0300

    Add files via upload

---
+++ MySGL Workbenk.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO users
+VALUES(
+"3","joão",31,6000);
+
+INSERT INTO users
+VALUES(
+"4","Romulo",31,6000);
+
+INSERT INTO users
+VALUES(
+"5","paulo",31,6000);
+
+select * from users;
+where name like "j%" and (id > "1" and id < "4");
